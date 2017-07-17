09baedad72aadbf20ffbc41a1304f2e1c6df7cc2
Andreas Schulz <andi.schulz@me.com>
2017-07-17 23:51:00 +0200

    Update to qlift-c-api 0.0.18

--- Sources/main.swift
+++ Sources/main.swift
@@ -17,8 +17,8 @@ func main() -> Int32 {
     pushButton.connectClicked { checked in
         print("Button clicked, now is \(checked)")
     }
-    let spacer1 = QSpacerItem(width: 20, height: 40, hPolicy: .Minimum, vPolicy: .Expanding)
-    let spacer2 = QSpacerItem(width: 20, height: 40, hPolicy: .Minimum, vPolicy: .Expanding)
+    let spacer1 = QSpacerItem(width: 20, height: 40, horizontalPolicy: .Minimum, verticalPolicy: .Expanding)
+    let spacer2 = QSpacerItem(width: 20, height: 40, horizontalPolicy: .Minimum, verticalPolicy: .Expanding)
     layout.add(widget: label)
     layout.add(item: spacer1)
     layout.add(widget: pushButton)
