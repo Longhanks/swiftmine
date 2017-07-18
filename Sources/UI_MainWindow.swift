a197e115182d3fb6d145f55411f68ec2a953f6b9
Andreas Schulz <andi.schulz@me.com>
2017-07-18 21:30:00 +0200

    Update to qlift-c-api 0.0.30

--- Sources/UI_MainWindow.swift
+++ Sources/UI_MainWindow.swift
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'UI_MainWindow.ui'
 **
-** Created by: Qlift User Interface Compiler version 0.0.27
+** Created by: Qlift User Interface Compiler version 0.0.30
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
