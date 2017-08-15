e09fa69650878f101b855f91e0073c2f7475b217
Andreas Schulz <andi.schulz@me.com>
2017-08-15 22:40:52 +0200

    NewGameDialog: Connect index changed event

--- Sources/NewGameDialog.swift
+++ Sources/NewGameDialog.swift
@@ -4,5 +4,12 @@ import Qlift
 class NewGameDialog: UI_NewGameDialog {
     init(parent: QWidget? = nil) {
         super.init(parent: parent, flags: .Sheet)
+        self.comboBoxDefaultModes.connectCurrentIndexChanged { (_: Int32) -> Void in
+            self.checkNewMode()
+        }
+    }
+
+    func checkNewMode() {
+        print(#function)
     }
 }
