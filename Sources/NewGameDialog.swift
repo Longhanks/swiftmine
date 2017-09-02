81618dca2af1aa49c87a9f30c37974f1e283bd80
Andreas Schulz <andi.schulz@me.com>
2017-09-02 20:21:36 +0200

    Finish implementation of NewGameDialog

--- Sources/NewGameDialog.swift
+++ Sources/NewGameDialog.swift
@@ -4,15 +4,41 @@ import Qlift
 class NewGameDialog: UI_NewGameDialog {
     init(parent: QWidget? = nil) {
         super.init(parent: parent, flags: .Sheet)
-        self.comboBoxDefaultModes.connectCurrentIndexChanged { (_: Int32) -> Void in
-            self.checkNewMode()
-        }
+        self.comboBoxDefaultModes.connectCurrentIndexChanged(to: self.checkNewMode)
         if let parentWidget = parent {
             self.move(to: parentWidget.window.frameGeometry.topLeft + parentWidget.window.rect.center - self.rect.center)
         }
     }
 
-    func checkNewMode() {
-        print(#function)
+    func checkNewMode(_ newMode: String) {
+        switch newMode {
+        case "Beginner":
+            self.spinBoxRows.enabled = false
+            self.spinBoxRows.value = 9
+            self.spinBoxColumns.enabled = false
+            self.spinBoxColumns.value = 9
+            self.spinBoxMines.enabled = false
+            self.spinBoxMines.value = 10
+        case "Intermediate":
+            self.spinBoxRows.enabled = false
+            self.spinBoxRows.value = 16
+            self.spinBoxColumns.enabled = false
+            self.spinBoxColumns.value = 16
+            self.spinBoxMines.enabled = false
+            self.spinBoxMines.value = 40
+        case "Expert":
+            self.spinBoxRows.enabled = false
+            self.spinBoxRows.value = 30
+            self.spinBoxColumns.enabled = false
+            self.spinBoxColumns.value = 16
+            self.spinBoxMines.enabled = false
+            self.spinBoxMines.value = 99
+        case "Custom":
+            self.spinBoxRows.enabled = true
+            self.spinBoxColumns.enabled = true
+            self.spinBoxMines.enabled = true
+        default:
+            fatalError("Unknown game mode.")
+        }
     }
 }
