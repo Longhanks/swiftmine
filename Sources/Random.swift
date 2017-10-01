1b1084c631e2066435567d80e096f8c4c1b6c86d
Andreas Schulz <andi.schulz@me.com>
2017-10-01 20:51:42 +0200

    Fix Linux random

--- Sources/Random.swift
+++ Sources/Random.swift
@@ -7,7 +7,7 @@ import Darwin
 
 func randint(bound: UInt32) -> Int {
     #if os(Linux)
-    return random() % (bound + 1)
+    return random() % (Int(bound) + 1)
     #else
     return Int(arc4random_uniform(bound + 1))
     #endif
